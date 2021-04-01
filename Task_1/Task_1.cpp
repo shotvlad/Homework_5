04ad59475b2c9d48825294112acfa52ca6606bc0
Владислав Шотов <shotov.vlad@mail.ru>
2021-04-01 18:15:07 +0300

    Homework_5

--- Task_1/Task_1.cpp
+++ Task_1/Task_1.cpp
@@ -1,5 +1,5 @@
 ﻿#include <iostream>
-#define N 10
+#define POINTS 10
 
 using namespace std;
 
@@ -7,18 +7,20 @@ using namespace std;
 с постоянным шагом по горизонтали. Найдите области (точки высот),
 невидимые для наблюдателя, находящегося в первой точке.*/
 
-double arrayEnter(double array[N]);
+void arrayEnter(double array[]);
 double enterStep(double number, int step);
-void systemSurveillance(double array[N]);
-bool isDetectionHighPoint(double max, double first, double value, int step);
+void systemSurveillance(double array[]);
+bool isDetectionHighPoint(double max, double value, double first, int step, int stepMax);
 int checkingInvisibleAreas(bool isCheak, double value, int step);
+bool isVisibilityFromAbove(double max, double value, double first, int step, int stepMax);
 void printPoints(int step, double value);
 double meaningMax(double max, double value);
+int stepMaximum(double max, double value, int stepMax, int step);
 void noInvisibleDots(int points);
 
 int main()
 {
-	double array[N];
+	double array[POINTS];
 
 	arrayEnter(array);
 
@@ -27,35 +29,37 @@ int main()
 	return 0;
 }
 
-double arrayEnter(double array[N])
+void arrayEnter(double array[])
 {
 	double number = 0;
 
-	for (int step = 0; step < N; step++)
+	for (int step = 0; step < POINTS; step++)
 	{
 		array[step] = enterStep(number, step);
 	}
-
-	return array[N];
 }
 
 double enterStep(double number, int step)
 {
 	cout << "Enter the height of the plain at point " << step + 1 << ": ";
 	cin >> number;
+
 	return number;
 }
 
-void systemSurveillance(double array[N])
+
+void systemSurveillance(double array[])
 {
-	int invisiblePoints = 0;
-	double max = array[0];
+	int invisiblePoints = 0, stepMax = 1;
+	double max = array[1];
 
-	cout << "Invisible points: ";
+	cout << "Invisible points: " << endl;
 
-	for (int step = 1; step < N; step++)
+	for (int step = 2; step < POINTS; step++)
 	{
-		invisiblePoints += checkingInvisibleAreas(isDetectionHighPoint(max, array[0], array[step], step), array[step], step);
+		invisiblePoints += checkingInvisibleAreas(isDetectionHighPoint(max, array[step], array[0], step, stepMax), array[step], step);
+
+		stepMax = stepMaximum(max, array[step], stepMax, step);
 
 		max = meaningMax(max, array[step]);
 	}
@@ -63,9 +67,26 @@ void systemSurveillance(double array[N])
 	noInvisibleDots(invisiblePoints);
 }
 
-bool isDetectionHighPoint(double max, double first, double value, int step)
+bool isDetectionHighPoint(double max, double value, double first, int step, int stepMax)
 {
-	if (((max >= value) and (step > 1)) and (max != first))
+	if (first >= value and first > max)
+	{
+		return isVisibilityFromAbove(max, value, first, step, stepMax);
+	}
+/*  else if (max == value and max == first) // Если принять, что наблюдатель стоя на первой точке имеет свою высоту
+	{
+		return false;
+	}*/
+	else if (max < value)
+	{
+		return false;
+	}
+	else return true;
+}
+
+bool isVisibilityFromAbove(double max, double value, double first, int step, int stepMax)
+{
+	if (stepMax / abs(first - max) >= step / abs(first - value)) // знак > если принять, что наблюдатель стоя на первой точке имеет свою высоту
 	{
 		return true;
 	}
@@ -97,10 +118,20 @@ double meaningMax(double max, double value)
 	else return value;
 }
 
+int stepMaximum(double max, double value, int stepMax, int step)
+{
+	if (max > value)
+	{
+		return stepMax;
+	}
+	else return step;
+}
+
 void noInvisibleDots(int points)
 {
 	if (points == 0)
 	{
 		cout << "there are no such things in the array" << endl;
 	}
+	else {}
 }
--- Task_2/Task_2.cpp
+++ Task_2/Task_2.cpp
@@ -1,1 +1,108 @@
-﻿
+﻿#include <iostream>
+#define ROOKIE 10
+
+using namespace std;
+
+/*НОВОБРАНЦЫ. Новобранцы выстроены в шеренгу. По команде НАЛЕ–ВО!
+одни из них поворачиваются налево, а другие – направо. 
+Это есть начальное положение шеренги. 
+Далее каждый новобранец действует следующим образом:
+увидев перед собой лицо соседа, он ровно через секунду поворачивается кругом.
+В конце концов повороты прекратятся.Тогда сержант подает звуковой сигнал.*/
+
+void rankEnter(char array[]);
+char enterStep(char number, int step);
+void turnsRank(char array[]);
+void rankCout(char array[]);
+int partingRookies(char array[], int rookies);
+int secondCout(int second);
+
+int main()
+{
+	char line[ROOKIE];
+
+	rankEnter(line);
+
+	turnsRank(line);
+
+	return 0;
+}
+
+
+void rankEnter(char array[])
+{
+	char number = 0;
+
+	for (int rookie = 0; rookie < ROOKIE; rookie++)
+	{
+		array[rookie] = enterStep(number, rookie);
+	}
+}
+
+char enterStep(char number, int rookie)
+{
+	cout << "Specify the turn of " << rookie + 1 << " recruit in the line (< or >): ";
+	cin >> number;
+
+	return number;
+}
+
+
+void turnsRank(char array[])
+{
+	int second = 1;
+
+	cout << "Starting position of the line: ";
+
+	while (true)
+	{
+		int rookies = 1;
+
+		rankCout(array);
+
+		rookies = partingRookies(array, rookies);
+
+		if (rookies == ROOKIE)
+		{
+			cout << "Signal!" << endl;
+			break;
+		}
+
+		second = secondCout(second);
+	}
+}
+
+void rankCout(char array[])
+{
+	for (int rookie = 0; rookie < ROOKIE; rookie++)
+	{
+		cout << array[rookie] << " ";
+	}
+	cout << endl;
+}
+
+int partingRookies(char array[], int rookies)
+{
+	for (int rookie = 1; rookie < ROOKIE; rookie++)
+	{
+		if ((array[rookie - 1] == '>') and (array[rookie] == '<'))
+		{
+			array[rookie - 1] = '<';
+			array[rookie] = '>';
+			rookie++;
+		}
+		else
+		{
+			rookies++;
+		}
+	}
+
+	return rookies;
+}
+
+int secondCout(int second)
+{
+	cout << "After " << second << " second: ";
+
+	return second + 1;
+}
